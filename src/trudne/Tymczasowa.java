1e790ecb1481233f117909141524bdc4b72fd9dc
Piotr Karol <teropod2601@gmail.com>
2023-02-24 18:11:16 +0100

    Initial commit

---
+++ src/trudne/Tymczasowa.java
@@ -0,0 +1,6 @@
+package trudne;
+
+public class Tymczasowa {
+    // tutaj pojawią się kody troszkę bardziej "zaawansowane" nie bede się tu bawić w wyjaśnianie wszystkiego
+    // bedę się tutaj starać napisać wszystko możliwie jak najkrócej żeby było jak najmniej do przepisywania na teście
+}
---
+++ src/łatwe/Tymczasowa2.java
@@ -0,0 +1,6 @@
+package łatwe;
+
+public class Tymczasowa2 {
+    // w tym pakiecie pojawią się kody łatwe do zrozumienia dla każdego. Dosłownie "chwila dla debila" z opisem wszystkiego co sie da
+    //kod będe się starał napisać w sposób dosłownie najprostrzy możliwy, ale może być dłuższy do przepisania na teście
+}
